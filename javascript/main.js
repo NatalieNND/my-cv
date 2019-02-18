41d3648b47943312f2f036b1491c0aabe2d6fe82
NatalieNND <nataliedaniels904@gmail.com>
2019-02-18 15:15:42 +0200

    created new cv

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML
+    = "I am a Junior Web Developer who is passionate about working with computers. Hardwork and dedication are my two main qualities when I try out something new that I want to pursue. I am a very outgoing person who loves spending time outdoors and enjoying nature. I have a passion for fashion so I love designing and drawing and I am always looking for different ways to put my creativity to the test. I am socially oriented, I have a strong self-motivation to get to know people in all walks of life and to nurture those relationships.  I have a natural enthusiasm for all types of ideas and projects - my own and other people's. People are likely to describe me as gregarious, persuasive and optimistic." 
+   
+}
